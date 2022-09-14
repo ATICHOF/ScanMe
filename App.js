e4a9cc59ad2abafd6067d7a9bb84661522188f01
ATICHOF <sof.atiche@gmail.com>
2022-09-14 12:21:43 +0100

    adding react navigation

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import TextRecognizer from './containers/TextRecognizer';
-
 const App = () => {
-  return <TextRecognizer />;
+  return (
+    <NavigationContainer>
+      <TextRecognizer />
+    </NavigationContainer>
+  );
 };
 
 export default App;
